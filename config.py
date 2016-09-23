7b501d80ee998dad41e3b40db3851bf2fadbc387
Nils Braun <nils.braun@kit.edu>
2016-09-23 19:32:13 +0200

    Added first draft for config

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
