3c810111bef8d23600ca5c63bf4329c52848bb2a
Nils Braun <nils.braun@kit.edu>
2016-10-09 11:16:59 +0200

    Removed an unused function.

--- app/game/functions.py
+++ app/game/functions.py
@@ -1,27 +1,3 @@
-from app import db
-
-
-def make_turn(turn_id, game, user):
-    if user != game.current_user:
-        raise AttributeError("It is not the users turn.")
-
-    possible_turns = game.get_possible_turns(user)
-
-    if turn_id >= len(possible_turns):
-        raise RuntimeError("Turn is not possible.")
-
-    turn = possible_turns[turn_id]
-    turn.set_turn_properties()
-
-    db.session.add(turn)
-    # TODO: Do we need a commit here?
-
-    game.update_game_status()
-    db.session.merge(game)
-
-    db.session.commit()
-
-
 class CachedClassFunction:
     def __init__(self, variable_name=None):
         self._variable_value = None
